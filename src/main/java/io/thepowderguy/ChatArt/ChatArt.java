93d559a99c5f8daf420458bbbbe583e6c84a2302
thepowderguy <brandonli2010@gmail.com>
2014-02-10 17:59:04 -0500

    set up main class

--- src/main/java/io/thepowderguy/ChatArt/ChatArt.java
+++ src/main/java/io/thepowderguy/ChatArt/ChatArt.java
@@ -1,1 +1,23 @@
-//init
+public class ChatArt extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+		saveDefaultConfig();
+    }
+
+	@Override
+	pubic void onDisable(){
+		saveConfig();
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("channel")){
+			if (args.length < 1)
+			{
+				return false;
+			}
+		}
+		return false;
+	}
+}
