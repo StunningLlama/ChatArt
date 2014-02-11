b3d08844c5433f0c1b9ace08dc3bb002383805b7
thepowderguy <brandonli2010@gmail.com>
2014-02-10 19:25:17 -0500

    add cmd

--- src/main/java/io/thepowderguy/ChatArt/ChatArt.java
+++ src/main/java/io/thepowderguy/ChatArt/ChatArt.java
@@ -13,9 +13,16 @@ public class ChatArt extends JavaPlugin {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(cmd.getName().equalsIgnoreCase("channel")){
-			if (args.length < 1)
+			if (args.length != 1)
 			{
 				return false;
+			}
+			if (getConfig().getStringList("art." + args[0]) != null)
+			{
+				for (Integer index = 0; index < getConfig().getStringList("art." + args[0]).size())
+				{
+					getServer().broadcastMessage(getConfig().getString("prefix") + getConfig().getStringList("art." + args[0]).get(index))
+				}
 			}
 		}
 		return false;
