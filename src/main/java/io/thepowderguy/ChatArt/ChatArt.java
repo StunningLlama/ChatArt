887799b7edca467929da01c116bc94709d5e838a
thepowderguy <brandonli2010@gmail.com>
2014-02-15 21:08:42 -0500

    tab complete

--- src/main/java/io/thepowderguy/ChatArt/ChatArt.java
+++ src/main/java/io/thepowderguy/ChatArt/ChatArt.java
@@ -9,6 +9,7 @@ public class ChatArt extends JavaPlugin {
 
 	@Override
 	public void onEnable(){
+		this.getCommand("art").setTabCompleter(this);
 		saveDefaultConfig();
 	}
 
@@ -35,5 +36,31 @@ public class ChatArt extends JavaPlugin {
 			}
 		}
 		return false;
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) 
+	{
+		List<String> toreturn = new ArrayList<String>();
+		toreturn.clear();
+		if (command.getName().equalsIgnoreCase("art"))
+		{
+			if (args.length == 1)
+			{
+				toreturn.addAll(this.getConfig().getConfigurationSection("art").getKeys(false));
+			}
+			for (int ind = 0; ind < toreturn.size();)
+			{
+				if (!toreturn.get(ind).toLowerCase().startsWith(args[args.length - 1].toLowerCase()))
+				{
+					toreturn.remove(ind);
+				}
+				else
+				{
+					ind++;
+				}
+			}
+		}
+		return toreturn;
 	}
 }
